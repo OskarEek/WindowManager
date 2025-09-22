16142b8cffc9aab0ef39ba1e508c7dbc255ed923
Oskar Eek <oskar.eek99@outlook.com>
2025-09-22 18:06:12 +0200

    Add programs to config

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -1,13 +1,55 @@
-﻿using WindowManager.Services;
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using WindowManager.Models;
+using WindowManager.Services;
 
 namespace WindowManager.ViewModels
 {
-    public class MainViewModel
+    public class MainViewModel : INotifyPropertyChanged
     {
-        
-        public MainViewModel()
+        private readonly ConfigService _configService;
+        private string? _programPath;
+        public ObservableCollection<ProcessModel> StoredPrograms { get; }
+        public string? ProgramPath
         {
+            get => _programPath;
+            set
+            {
+                if (_programPath != value)
+                {
+                    _programPath = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ProgramPath)));
+                }
+            }
         }
 
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public MainViewModel(ConfigService configSerivce)
+        {
+            _configService = configSerivce;
+            StoredPrograms = new ObservableCollection<ProcessModel>();
+            RefreshStoredPrograms();
+        }
+
+        public void AddProgramButton()
+        {
+            if (string.IsNullOrEmpty(_programPath))
+                return;
+
+            _configService.AddProgram(_programPath);
+
+            RefreshStoredPrograms();
+            ProgramPath = "";
+        }
+
+        public void RefreshStoredPrograms()
+        {
+            Config config = _configService.GetConfig();
+            StoredPrograms.Clear();
+            foreach (ProcessModel program in config.Programs)
+            {
+                StoredPrograms.Add(program);
+            }
+        }
     }
 }
